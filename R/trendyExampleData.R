7c2871b6569f5e5ecdc87bfc41e5d2a74c66c661
Rhonda Bacher <rhonda.bacher@gmail.com>
2018-01-31 14:58:56 -0500

    change .R name, and add biocView: Regression

---
+++ R/trendyExampleData.R
@@ -0,0 +1,14 @@
+#' Example dataset for Trendy
+#'
+#' Example time-course dataset.
+#'
+#' @docType data
+#'
+#' @usage data(trendyExampleData)
+#'
+#' @format data matrix
+#'
+#' @keywords datasets
+#'
+#' @examples data(trendyExampleData)
+"trendyExampleData"
