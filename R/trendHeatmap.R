b4d12ebd9fa10671a2ba0cf90596c185e3d5d6ef
Eliza Morton <elizarmorton@yahoo.com>
2020-07-06 17:24:47 -0400

    error fixed in trendHeatmap

--- R/trendHeatmap.R
+++ R/trendHeatmap.R
@@ -26,8 +26,7 @@
 #'  topGenes <- topTrendy(myTrends)
 #'  #makeHeat <- trendHeatmap(topGenes)
 
-trendHeatmap <- 
-    function (topTrendyData, featureNames=NULL, cexRow = .5, cexCol=.5)
+trendHeatmap <- function (topTrendyData, featureNames=NULL, cexRow = .5, cexCol=.5)
     
 {
     bks.all <- topTrendyData$Breakpoints
@@ -35,35 +34,39 @@ trendHeatmap <-
     bks.first <- bks.all[,1]
     names(bks.first) <- rownames(topTrendyData[[1]])
     nobp <- names(which(is.na(bks.first)))
-    bks.first[nobp] <- max(bks.all, na.rm=TRUE) + 1
-    bks.first.sort <- sort(bks.first)
+    
+    if (any(!is.na(bks.first))) { 
+        bks.first[nobp] <- max(bks.all, na.rm=TRUE) + 1
+        bks.first.sort <- sort(bks.first)
+    } else { 
+        bks.first[is.na(bks.first)] <- 0}
     
     seg.all.id <- topTrendyData[[1]]
     seg.first <- seg.all.id[,1]
     
     genes <- rownames(seg.all.id)
     if(!is.null(featureNames)) {
-         genes <- intersect(genes, featureNames) 
-     }
+        genes <- intersect(genes, featureNames) 
+    }
     bks.sign.sort.list <- list(
         firstup = sort(bks.first[genes[which(seg.first == 1)]]),
         firstdown = sort(bks.first[genes[which(seg.first == -1)]]),
         firstnochange = sort(bks.first[genes[which(seg.first == 0)]]))
-        
+    
     bks.sign.sort <- c(bks.sign.sort.list[[1]], 
-    bks.sign.sort.list[[2]],bks.sign.sort.list[[3]])
+                       bks.sign.sort.list[[2]],bks.sign.sort.list[[3]])
     
     seg.all.id.highr.sign.sort <- seg.all.id[names(bks.sign.sort),]
     
     colnames(seg.all.id.highr.sign.sort) <- gsub(".Fitted.Trend", "", 
-    colnames(seg.all.id.highr.sign.sort), fixed=TRUE)
+                                                 colnames(seg.all.id.highr.sign.sort), fixed=TRUE)
     
     gplots::heatmap.2(seg.all.id.highr.sign.sort, trace = "none",
-        Rowv = FALSE, Colv = FALSE, col = c("slateblue1", "black", "tomato"),
-        key = FALSE, cexRow = cexRow, cexCol=cexCol,
-		dendrogram='none')
+                      breaks = c(-2,-1,0,1),
+                      Rowv = FALSE, Colv = FALSE, col = c("slateblue1", "black", "tomato"),
+                      key = FALSE, cexRow = cexRow, cexCol=cexCol,
+                      dendrogram='none')
     legend("top", ncol = 3, c("up", "no change", "down"), 
-        fill = rev(c("slateblue1", "black", "tomato")))
-        
+           fill = rev(c("slateblue1", "black", "tomato")))
+    
     return(bks.sign.sort.list)
-}
